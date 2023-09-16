df6172dfafbd3a83c4342891309c672e9e33aff4
RutvaDudhela11 <rutvadud158@gmail.com>
2023-09-16 17:20:52 +0530

    solution1 for problem1:Program to count occurrences of a given character in a string.

---
+++ Solution1.py
@@ -0,0 +1,21 @@
+# Problem 1: Program to count occurrences of a given character in a string.
+
+# Count character 'e' in the below string.
+# 	Input "geeksforgeeks". 
+#	Output: 4
+
+input_1 = "geeksforgeeks"
+
+x = input_1.count("e")
+
+print(x)
+
+# Count character 'a' in the below string.
+# 	Input "abccdefgaa."
+# 	Output : 3
+
+input_2 = "abccdefgaa."
+
+y = input_2.count("a")
+
+print(y)
