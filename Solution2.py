21757b5a9fbc61c3557007569d91dad83551110a
RutvaDudhela11 <rutvadud158@gmail.com>
2023-09-16 17:22:17 +0530

    solution for problem2: Determine whether a given string is Palindrome

---
+++ Solution2.py
@@ -0,0 +1,11 @@
+#  Problem 2:  Determine whether a given string is Palindrome
+
+def palindrome_function(initial_string):
+    reverse_string = ""
+    
+    for char in initial_string:
+        # print(char)
+        x = reverse_string  + char
+        
+    
+palindrome_function("Hello")
